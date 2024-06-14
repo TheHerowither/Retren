b7f7a5f586f62ddcc1ef0bb3c7b821668aaa2e9b
TheHerowither <lucland278@gmail.com>
2024-06-14 18:10:25 +0200

    Remove dead code

--- src/main.c
+++ src/main.c
@@ -1,21 +1,11 @@
 #include <retren.h>
-#include <signal.h>
 
 
-
-
-retren_Window window;
-void quit(int dummy) {
-    (void)dummy;
-    window.is_open = false;
-}
 
 int main(void) {
-    signal(SIGINT, quit);
-
     retren_Init();
 
-    window = retren_CreateWindow("Test window", 1080, 720);
+    retren_Window window = retren_CreateWindow("Test window", 1080, 720);
 
     while (window.is_open) {
         retren_Update(&window);
